e453c0c931293c4a638555ace867e0518ffae219
priyasahu <sahu.priya488@gmail.com>
2018-01-21 21:40:19 +0530

    current user logged in etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user , except: [:index, :show]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 2)
   end
